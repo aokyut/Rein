48b5c1f7864bb01839155f22bae9536d5134f8bc
aokyut <epaperamic@gmail.com>
2021-02-14 11:48:42 +0900

    [add] BinaryFunction
    二項演算用の抽象クラスBinaryFunctionを追加

---
+++ Scripts/Runtime/Functions/Base/BinaryFunction.cs
@@ -0,0 +1,14 @@
+﻿namespace Rein.Functions
+{
+    public abstract class BinaryFunction: BaseFunction
+    {
+        protected abstract Tensor BinaryForward(Tensor tensor1, Tensor tensor2);
+        protected abstract void BinaryBackward();
+
+        public BinaryFunction()
+        {
+            this.FunctionForward = (tensor) => new Tensor[1]{ this.BinaryForward(tensor[0], tensor[1]) };
+            this.FunctionBackward = this.BinaryBackward;
+        }
+    }
+}
