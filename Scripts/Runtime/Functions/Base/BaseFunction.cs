7861ebc95c26f5b45382207bd7c187c2794ac3d7
aokyut <epaperamic@gmail.com>
2021-02-14 11:45:08 +0900

    [add] BaseFunction
    Functionsの基礎となるBaseFunctionを作成。

---
+++ Scripts/Runtime/Functions/Base/BaseFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Rein.Functions
+{
+    public abstract class BaseFunction: IFunction
+    {
+        protected Tensor[] Inputs, Outputs;
+        protected int UseCount = 0;
+        
+        protected Func<Tensor[], Tensor[]> FunctionForward;
+        protected Action FunctionBackward;
+
+        public Tensor[] Forward(params Tensor[] inputs)
+        {
+            this.Inputs = inputs;
+            this.Outputs = this.FunctionForward(inputs);
+            this.UseCount = this.Outputs.Length;
+
+            return this.Outputs;
+        }
+
+        public void Backward()
+        {
+            this.UseCount--;
+            if (this.UseCount != 0)return;
+            this.FunctionBackward();
+        }
+    }
+}
