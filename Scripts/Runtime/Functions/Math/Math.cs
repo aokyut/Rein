8d5c9bba888d4e4cc1ba86d258a0ffba853c1d7f
aokyut <epaperamic@gmail.com>
2021-02-18 20:23:32 +0900

    [add] Rein.Functions.Mathクラスを追加。MinusとReLU, Exp, Logを追加
    算術演算クラスのRein.Functions.Mathクラスを追加。
    MinusとReLU, Exp, Logを実装した。
    ここにLambda計算で書くことのできるクラスを記述する予定。

---
+++ Scripts/Runtime/Functions/Math/Math.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Rein.Functions{
+    public static class Math{
+        
+        // -X
+        public static Tensor[] Minus(Tensor[] In){
+            return new Lambda(
+                "Minus",
+                (x) => -x,
+                (x) => -1
+            ).Forward(In);
+        }
+
+        // ReLU
+        public static Tensor[] ReLU(Tensor[] In){
+            return new Lambda(
+                "ReLU",
+                (x) => x > 0 ? x : 0,
+                (x) => x > 0 ? 1 : 0
+            ).Forward(In);
+        }
+
+        // Exponential
+        public static Tensor[] Exp(Tensor[] In){
+            return new Lambda(
+                "Exp",
+                (x) => System.Math.Exp(x),
+                (x) => System.Math.Exp(x)
+            ).Forward(In);
+        }
+
+        // Log
+        public static Tensor[] Log(Tensor[] In){
+            return new Lambda(
+                "Log_e",
+                (x) => System.Math.Log(x),
+                (x) => 1 / x
+            ).Forward(In);
+        }
+    }
+}
