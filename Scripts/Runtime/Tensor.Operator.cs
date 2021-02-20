35de5d36fb53ef5f7785b5507cffe45b1353272e
aokyut <epaperamic@gmail.com>
2021-02-21 01:49:20 +0900

    [modify] ディレクトリ構造の変更によるエラーを解消

--- Scripts/Runtime/Tensor.Operator.cs
+++ Scripts/Runtime/Tensor.Operator.cs
@@ -18,7 +18,7 @@ namespace Rein
 
         public static Tensor operator -(Tensor tensor)
         {
-            return Functions.Math.Minus(tensor);
+            return F.Minus(tensor);
         }
 
         public static Tensor operator *(Tensor tensor1, Tensor tensor2)
