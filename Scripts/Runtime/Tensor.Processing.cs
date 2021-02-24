7c1550b4a48bd832c060c5924293546b7f8c24bb
aokyut <epaperamic@gmail.com>
2021-02-24 13:57:35 +0900

    [feat] Tensor操作関数を呼び出すための関数を追加。

--- Scripts/Runtime/Tensor.Processing.cs
+++ Scripts/Runtime/Tensor.Processing.cs
@@ -1,4 +1,6 @@
 ﻿using Rein.Functions.Process;
+using System.Collections.Generic;
+
 
 namespace Rein
 {
@@ -6,6 +8,27 @@ namespace Rein
     {
         public Tensor Repeat(int dim, int rep){
             return new Repeat(dim, rep).Forward(this);
+        }
+
+        public Tensor Detach(){
+            return new Detach().Forward(this);
+        }
+
+        public Tensor Squeeze(int dim){
+            return new Squeeze(dim).Forward(this);
+        }
+
+        // 全部の要素についてSqueezeを試みる
+        public Tensor Squeeze(){
+            return new SqueezeAll().Forward(this);
+        }
+        
+        public Tensor  Unsqueeze(int dim = 0){
+            return new Unsqueeze(dim).Forward(this);
+        }
+
+        public Tensor Reshape(List<int> shape){
+            return new Reshape(shape).Forward(this);
         }
     }
 }
